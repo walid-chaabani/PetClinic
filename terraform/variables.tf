6276925cbf1feefe3805fcabd4e626f74b96dd7b
Walid Chaâbani <92535419+walid-chaabani@users.noreply.github.com>
2024-02-09 15:17:00 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "petclinic-rg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "petclinic"
 }
 
 variable "location" {
